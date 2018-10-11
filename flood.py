a0b0ae65ceb86a3c8c47ccccf33592a9024eec8f
Diegobug <diegoifrn@gmail.com>
2018-10-11 19:38:11 -0300

    Add files via upload

--- flood.py
+++ flood.py
@@ -1,66 +1,65 @@
 import pygame
-import sys
 import random
-import numpy
 
+#função de flood
+def flood(cor_atual,matriz,i,j,nova_cor):
+    if(nova_cor == cor_atual or cor_atual!= matriz[i][j]):
+        return
+    elif(0<=i<6 and 0<=j<6):
+        matriz[i][j] = nova_cor
+        flood(cor_atual,matriz,i+1,j,nova_cor)# direita
+        flood(cor_atual,matriz,i,j+1,nova_cor)# baixo
+        flood(cor_atual,matriz,i,j-1,nova_cor)# esquerda
+        flood(cor_atual,matriz,i-1,j,nova_cor)# cima
+        
 pygame.init()
-#declaracao de variaveis
-screen = pygame.display.set_mode((640,480))
-pygame.display.set_caption("flood fill")
+#criando as cores  
+white = (255,255,255)
+black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
-darkBlue = (0,0,128)
-white = (255,255,255)
-black = (0,0,0)
-pink = (255,200,200)
-screen.fill(pink)
-colors = [red,green,blue,darkBlue,white,black] #cores
-matriz = [[0 for x in range(10)] for y in range(10)] #matriz
+#configurando a tela
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption("flood")
+screen.fill(white)
+#criando a matriz
+matriz = [[0 for x in range(7)] for y in range(7)]
+#criando um vetor de cores
+vetorCores = [black,red,green,blue]
+#texto
+jogada = 0
+fonte = pygame.font.SysFont("Comic Sams MS", 30)
+texto = fonte.render("Jogada "+str(jogada),False,(0,0,0))
+#preenche a matriz com numeros aleatorios
+for i in range(6):
+    for j in range(6):
+        matriz[i][j] = random.randint(0,3)
+print(matriz)
 
-#funcao de floodar
-def flood(cor_alvo,matriz,i,j,nova_cor):
-    if(matriz[i][j]!=cor_alvo or cor_alvo == nova_cor):
-        return
-    elif(0<=i<9 and 0<=j<9):
-        matriz[i][j] = nova_cor
-        flood(cor_alvo,matriz,i+1,j,nova_cor)
-        flood(cor_alvo,matriz,i,j+1,nova_cor)
-        flood(cor_alvo,matriz,i-1,j,nova_cor)
-        flood(cor_alvo,matriz,i,j-1,nova_cor)
+controle = True
+while controle:
+    screen.fill(white)
+    for evento in pygame.event.get():
+        if(evento.type == pygame.QUIT):
+            controle = False
+        for i in range(6):
+            for j in range(6):
+                rect = (i*50,j*50,50,50)
+                pygame.draw.rect(screen,vetorCores[matriz[i][j]],rect,0)
 
-#preenche a matriz de forma aleatoria       
-for i in range(0,9):
-    for j in range(0,9):
-        matriz[i][j] = random.randint(0,5)
-        
-sair = True
+        if (evento.type == pygame.MOUSEBUTTONUP):
+            x, y = pygame.mouse.get_pos() 
+            flood(matriz[0][0],matriz,0,0,matriz[x//50][y//50])
+            jogada = jogada+1
+            texto = fonte.render("Jogada "+str(jogada),False,(0,0,0))
 
-while sair:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sair = False   
-        for i in range(0,9):
-            for j in range(0,9):
-                pygame.draw.rect(screen, colors[matriz[i][j]], (i*50,j*50,50,50), 0)
+        screen.blit(texto,(0,310))
+        pygame.display.update()            
+pygame.quit() 
 
-        keys=pygame.key.get_pressed()
-        if keys[pygame.K_0]:
-            flood(matriz[0][0],matriz,0,0,0)
-        if keys[pygame.K_1]:
-            flood(matriz[0][0],matriz,0,0,1)
-        if keys[pygame.K_2]:
-            flood(matriz[0][0],matriz,0,0,2)
-        if keys[pygame.K_3]:
-            flood(matriz[0][0],matriz,0,0,3)
-        if keys[pygame.K_4]:
-            flood(matriz[0][0],matriz,0,0,4)
-        if keys[pygame.K_5]:
-            flood(matriz[0][0],matriz,0,0,5)    
-        pygame.display.update()
-    
-pygame.quit()
 
-    
+
+
 
 
